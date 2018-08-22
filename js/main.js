ea3f3c622eed25ee419ba711d0bd9dae1cadd9e1
ostlerb <39224251+ostlerb@users.noreply.github.com>
2018-08-22 23:02:06 +0200

    n/a

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-$(document).ready( function() {
+$(window).on("load", function() 
+{
 	$(".jumbotron").delay(500).animate({"opacity": "1"}, 500);
     $("#navBox").delay(500).animate({"opacity": "1"}, 1000);
 });
