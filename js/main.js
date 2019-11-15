dc30420ffa4814cae115372d3fce6a542f8a41d9
ostlerb <39224251+ostlerb@users.noreply.github.com>
2019-11-14 22:15:03 -0700

    Update 2 11/14/19

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(window).on("load", function()
 	$(".jumbotron").delay(500).animate({"opacity": "1"}, 500);
     $("#navBox").delay(500).animate({"opacity": "1"}, 1000);
 });
+
+function overflowAuto()
+{
+	document.body.style.overflow = "auto";
+}
+
+function overflowHidden()
+{
+	document.body.style.overflow = "hidden";
+}
