8c3db8a25fd13f38937fbcf80956173760b7a983
ostlerb <39224251+ostlerb@users.noreply.github.com>
2019-12-29 18:35:57 -0700

    Update 12/29/19

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+function isMobile()
+{
+	if (/Mobi|Android/i.test(navigator.userAgent)) 
+	{
+		return true;
+	}
+	
+	return false;
+}
 
 $(window).on("load", function() 
 {
+	mobile = isMobile();
+
+	if (mobile)
+	{
+		document.body.style.overflow = "auto";
+	}
+	
 	$(".jumbotron").delay(500).animate({"opacity": "1"}, 500);
     $("#navBox").delay(500).animate({"opacity": "1"}, 1000);
 });
@@ -12,5 +28,9 @@ function overflowAuto()
 
 function overflowHidden()
 {
-	document.body.style.overflow = "hidden";
+	mobile = isMobile();
+	if (!mobile)
+	{
+		document.body.style.overflow = "hidden";
+	}
 }
